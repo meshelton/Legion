1c9735237106567f81c5b2787df78cc4c1bd6d52
Michael Shelton <michael@indify.io>
2024-06-03 13:58:51 -0700

    Adds VelocityMatch steering behavior

---
+++ Character/Movement/Steering/VelocityMatch.cs
@@ -0,0 +1,48 @@
+using Godot;
+using Legion.Character.Movement.Kinematic;
+
+namespace Legion.Character.Movement.Steering;
+
+[Tool]
+[GlobalClass, Icon("res://Icons/arrive.svg")]
+public partial class VelocityMatch : SteeringBehavior3D
+{
+    private Node3D _target;
+    private KinematicTracker _tracker = new();
+
+    [Export]
+    public Node3D Target
+    {
+        get => _target;
+        set
+        {
+            _target = value;
+            _tracker.Track(_target);
+        }
+    }
+
+    [Export] public float MaxAcceleration;
+    [Export] public float TimeToTarget = 0.1f;
+
+    public override void _Process(double delta)
+    {
+        _tracker.Update((float) delta);
+    }
+
+
+    public override SteeringOutput3D GetSteering()
+    {
+        SteeringOutput3D result = new();
+
+        result.Linear = _tracker.Velocity - CharacterController.Velocity;
+        result.Linear /= TimeToTarget;
+
+        if (result.Linear.Length() > MaxAcceleration)
+        {
+            result.Linear = result.Linear.Normalized() * MaxAcceleration;
+        }
+
+        result.Angular = 0;
+        return result;
+    }
+}
